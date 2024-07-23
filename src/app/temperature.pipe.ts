2f4b75dfc8eae7443c66ddeccae970daadbebf9f
TheoCaesar <theocaesar7@gmail.com>
2024-07-23 04:25:07 +0100

    perform custom transformations

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -5,7 +5,10 @@ import { Pipe, PipeTransform } from "@angular/core";
     standalone: true
 })
 export class TemperaturePipe implements PipeTransform{
-    transform(value: any , ...args: any[]){
-        return `temp ${value} - ${args}`
+    transform(value: string | number , ...args: any[]){
+        let val = (typeof value=== "string") ? parseFloat(value) :  value;
+
+        const outputTemp = val * (9/5) + 32; //convert value to F
+        return `${outputTemp.toFixed(2)} °F`
     }
 }       
