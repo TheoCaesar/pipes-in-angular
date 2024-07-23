76bc54aa66400e71f9f2aa3dbb602e0b2ccbc018
TheoCaesar <theocaesar7@gmail.com>
2024-07-23 06:07:18 +0100

    build sort pipes

---
+++ src/app/sort.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort',
+  standalone: true
+})
+export class SortPipe implements PipeTransform {
+  transform(value: string[] | number[], direction: 'asc' | 'desc' = 'asc'){
+    const sortedArray = [...value];
+    sortedArray.sort((a,b)=>{
+      if (direction === 'asc') {
+        return a> b ? 1 : -1
+      } else return a > b ? -1 : 1
+    })
+    return sortedArray;
+  }
+
+}
